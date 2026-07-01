00440a61ffab0f0b1387382a7038b5d27e7c2a1c
ajrollerson <alistair.jrollerson@hotmail.co.uk>
2026-07-01 16:26:26 +0700

    Add REPL

--- src/repl.ts
+++ src/repl.ts
@@ -1,6 +1,27 @@
+import { createInterface } from "readline";
+
 export function cleanInput(input: string): string[] {
   if (typeof input !== "string") {
     throw new Error("Input must be a string!");
   }
   return input.toLowerCase().trim().split(" ").filter((word) => word !== "");
+}
+
+export function startREPL () {
+  const rl = createInterface({
+    input: process.stdin,
+    output: process.stdout,
+    prompt: "Pokedex > "
+  });
+
+  rl.prompt()
+  rl.on("line", (input: string) => {
+  const words = cleanInput(input)
+  if (words.length === 0) {
+    rl.prompt()
+    return
+  }
+  console.log(`Your command was: ${words[0]}`)
+  rl.prompt()
+});
 }
