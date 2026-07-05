260d833b05728467f17596b15443a1f2044df8e0
ajrollerson <alistair.jrollerson@hotmail.co.uk>
2026-07-05 18:03:21 +0700

    Add tests to pokecache.test.ts

--- src/pokecache.test.ts
+++ src/pokecache.test.ts
@@ -1,5 +1,6 @@
 import { Cache } from "./pokecache.js";
-import { test, expect } from "vitest";
+import { vi, test, expect } from "vitest";
+import { PokeAPI } from "./pokeapi.js";
 
 test.concurrent.each([
   {
@@ -12,6 +13,12 @@ test.concurrent.each([
     val: "moretestdata",
     interval: 1000, // 1 second
   },
+  {
+  key: "https://pokeapi.co/api/v2/location-area",
+  val: { count: 1, next: "", previous: "", results: [{ name: "pallet-town", url: "..." }] },
+  interval: 500,
+  },
+
 ])("Test Caching $interval ms", async ({ key, val, interval }) => {
   const cache = new Cache(interval);
 
@@ -24,4 +31,48 @@ test.concurrent.each([
   expect(reaped).toBe(undefined);
 
   cache.stopReapLoop();
+})
+
+test("adding the same key twice overwrites the first value", () => {
+  const cache = new Cache(5000);
+  
+  cache.add("https://example.com", "first");
+  cache.add("https://example.com", "second");
+  
+  const result = cache.get("https://example.com");
+  expect(result).toBe("second");
+  
+  cache.stopReapLoop();
 });
+
+test("adding a blank key", () => {
+  const cache = new Cache(5000);
+  
+  cache.add("", "example");
+  
+  const result = cache.get("");
+  expect(result).toBe("example");
+  
+  cache.stopReapLoop();
+});
+
+test("an empty value", () => {
+  const cache = new Cache(5000);
+  
+  cache.add("https://example.com", "");
+  
+  const result = cache.get("https://example.com");
+  expect(result).toBe("");
+  
+  cache.stopReapLoop();
+});
+
+
+
+
+
+
+
+
+
+;
